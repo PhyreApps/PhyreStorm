6b6e66c9f2c02fcaabaf43cb2c42b2603078a34a
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2025-02-10 11:24:43 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,37 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.9"
 }
 
 group = "org.phyreapps"
 version = "1.2-SNAPSHOT"
 
+
 repositories {
+
     mavenCentral()
+    google()
+    maven { url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies") }
+    maven { url = uri("https://plugins.jetbrains.com/maven") }
+    maven { url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
-
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.3.2.1")
-    type.set("PS") // Target IDE Platform
-
-    plugins.set(listOf(
-        "com.jetbrains.php:243.23654.168"
-    ))
-}
-
 
 dependencies {
-    implementation("org.jetbrains:annotations:24.0.0") // JetBrains annotations
-}
+    implementation("org.jetbrains:annotations:24.0.0")
 
-tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+    intellijPlatform {
+        phpstorm("2024.3.2.1", useInstaller = true)
+        bundledPlugin("com.jetbrains.php")
 
-    patchPluginXml {
-        sinceBuild.set("241")
-        untilBuild.set("243.*")
-    }
+        pluginVerifier()
+        zipSigner()
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+//        testFramework(TestFrameworkType.Platform)
     }
 }
